420a8792ba216cb1897729f86f322776e3063bd1
Homo Efficio <homo.efficio@gmail.com>
2019-10-07 00:13:09 +0900

    Add dummy RandomGeneratorServiceImpl [3-3]

---
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/service/RandomGeneratorServiceImpl.java
@@ -0,0 +1,16 @@
+package io.homo_efficio.learnmicroservicesspringboot.multiplication.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-06
+ */
+@Service
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+
+    @Override
+    public int generateRandomFactor() {
+        return 0;
+    }
+}
