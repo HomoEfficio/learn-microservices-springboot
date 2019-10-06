adcfc3cac98b13b3717a23192d7d784ae2bc2ec4
Homo Efficio <homo.efficio@gmail.com>
2019-10-06 11:33:09 +0900

    Initial Commit

---
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/LearnMicroservicesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.learnmicroservicesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMicroservicesSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMicroservicesSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/homo_efficio/learnmicroservicesspringboot/LearnMicroservicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.learnmicroservicesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMicroservicesSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
