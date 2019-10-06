31106585f60e5ffd70ea10efb5be9063ed8c56e1
Homo Efficio <homo.efficio@gmail.com>
2019-10-07 08:20:16 +0900

    Apply lombok to Multiplication [3-8]

--- src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/domain/Multiplication.java
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/domain/Multiplication.java
@@ -1,31 +1,24 @@
 package io.homo_efficio.learnmicroservicesspringboot.multiplication.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 /**
  * @author homo.efficio@gmail.com
  * created on 2019-10-06
  */
 @Getter
-public class Multiplication {
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
 
-    private int factorA;
-    private int factorB;
+    private final int factorA;
+    private final int factorB;
 
-    private int result;
-
-    public Multiplication(int factorA, int factorB) {
-        this.factorA = factorA;
-        this.factorB = factorB;
-        this.result = factorA * factorB;
-    }
-
-    @Override
-    public String toString() {
-        return "Multiplication{" +
-                "factorA=" + factorA +
-                ", factorB=" + factorB +
-                ", result=" + result +
-                '}';
+    public Multiplication() {
+        this(0, 0);
     }
 }
