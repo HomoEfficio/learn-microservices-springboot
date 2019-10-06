f0245028a87f0c3ea870dd19e207e4e2a8cb9987
Homo Efficio <homo.efficio@gmail.com>
2019-10-06 23:39:28 +0900

    Add MultiplicationServiceImpl but test still fails because there is no impl for RandomGeneratorService

---
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/service/MultiplicationServiceImpl.java
@@ -0,0 +1,23 @@
+package io.homo_efficio.learnmicroservicesspringboot.multiplication.service;
+
+import io.homo_efficio.learnmicroservicesspringboot.multiplication.domain.Multiplication;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-06
+ */
+@Service
+@RequiredArgsConstructor
+public class MultiplicationServiceImpl implements MultiplicationService {
+
+    private final RandomGeneratorService randomGeneratorService;
+
+    @Override
+    public Multiplication createRandomMultiplication() {
+        int factorA = randomGeneratorService.generateRandomFactor();
+        int factorB = randomGeneratorService.generateRandomFactor();
+        return new Multiplication(factorA, factorB);
+    }
+}
