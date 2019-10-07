bf044604218cebea1c96021fa6c21996d3191873
Homo Efficio <homo.efficio@gmail.com>
2019-10-07 21:21:56 +0900

    Add PostMapping to MultiplicationAttemptController [3-20]

--- src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/controller/MultiplicationAttemptController.java
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/controller/MultiplicationAttemptController.java
@@ -1,9 +1,13 @@
 package io.homo_efficio.learnmicroservicesspringboot.multiplication.controller;
 
+import io.homo_efficio.learnmicroservicesspringboot.multiplication.domain.MultiplicationAttempt;
 import io.homo_efficio.learnmicroservicesspringboot.multiplication.service.MultiplicationService;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MultiplicationAttemptController {
 
     private final MultiplicationService multiplicationService;
+
+    @PostMapping
+    public ResponseEntity<ResultResponse> postResult(@RequestBody MultiplicationAttempt attempt) {
+        return ResponseEntity.ok(
+                new ResultResponse(multiplicationService.checkAttempt(attempt))
+        );
+    }
 
     @Getter
     @RequiredArgsConstructor
