26f8013318ef1347b1cc6ea0d49de6869ad7e3b8
Homo Efficio <homo.efficio@gmail.com>
2019-10-07 09:40:14 +0900

    Add getRandomMultiplication() to MultiplicationController to pass the test [3-17]

--- src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/controller/MultiplicationController.java
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/controller/MultiplicationController.java
@@ -1,7 +1,10 @@
 package io.homo_efficio.learnmicroservicesspringboot.multiplication.controller;
 
+import io.homo_efficio.learnmicroservicesspringboot.multiplication.domain.Multiplication;
 import io.homo_efficio.learnmicroservicesspringboot.multiplication.service.MultiplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  * created on 2019-10-07
  */
 @RestController
+@RequestMapping("/multiplications")
 @RequiredArgsConstructor
 public class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
+
+    @GetMapping("/random")
+    public Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
+    }
 }
