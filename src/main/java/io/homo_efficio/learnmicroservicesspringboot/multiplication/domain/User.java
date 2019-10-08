59bc99662455c8c1cadbdf148029287973b2daf2
Homo Efficio <homo.efficio@gmail.com>
2019-10-08 19:33:37 +0900

    Make User a JPA entity [3-34]

--- src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/domain/User.java
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/domain/User.java
@@ -5,18 +5,27 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 /**
  * @author homo.efficio@gmail.com
  * created on 2019-10-07
  */
+@Entity
+@Table(name = "USERS")
 @Getter
 @RequiredArgsConstructor
 @ToString
 @EqualsAndHashCode
 public final class User {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
     private final String alias;
 
+    // empty constructor for JSON/JPA
     public User() {
         this.alias = null;
     }
