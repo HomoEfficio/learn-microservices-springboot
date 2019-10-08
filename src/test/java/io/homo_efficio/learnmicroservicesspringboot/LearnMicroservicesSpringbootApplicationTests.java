863a39e06eab0975ef569af0172f59fbcaa7cff0
Homo Efficio <homo.efficio@gmail.com>
2019-10-08 09:00:11 +0900

    Exclude meaningless test LearnMicroservicesSpringbootApplicationTests

--- src/test/java/io/homo_efficio/learnmicroservicesspringboot/LearnMicroservicesSpringbootApplicationTests.java
+++ src/test/java/io/homo_efficio/learnmicroservicesspringboot/LearnMicroservicesSpringbootApplicationTests.java
@@ -3,10 +3,10 @@ package io.homo_efficio.learnmicroservicesspringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LearnMicroservicesSpringbootApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
