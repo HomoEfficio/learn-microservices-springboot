6cba56a85424e037fba539e93032dcc12fb6e42b
Homo Efficio <homo.efficio@gmail.com>
2019-10-08 19:37:02 +0900

    Make MultiplicationAttempt a JPA entity [3-35]

--- src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/domain/MultiplicationAttempt.java
+++ src/main/java/io/homo_efficio/learnmicroservicesspringboot/multiplication/domain/MultiplicationAttempt.java
@@ -5,23 +5,38 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 /**
  * Identifies the attempt from a {@link User} to solve a {@link Multiplication}.
  *
  * @author homo.efficio@gmail.com
  * created on 2019-10-07
  */
+@Entity
+@Table
 @Getter
 @RequiredArgsConstructor
 @ToString
 @EqualsAndHashCode
 public final class MultiplicationAttempt {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MULTIPLICATION_ATTEMPT_ID")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "USER_ID")
     private final User user;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "MULTIPLICATION_ID")
     private final Multiplication multiplication;
+
     private final int resultAttempt;
     private final boolean correct;
 
+    // empty constructor for JSON/JPA
     public MultiplicationAttempt() {
         this.user = null;
         this.multiplication = null;
