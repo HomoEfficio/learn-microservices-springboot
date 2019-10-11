5fc468729e79c3f694f1788c030f48f84418c67a
Homo Efficio <homo.efficio@gmail.com>
2019-10-11 18:55:19 +0900

    Add Badge enum [4-7]

---
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/domain/Badge.java
@@ -0,0 +1,17 @@
+package io.homo_efficio.lmsb.gamification.domain;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-11
+ */
+public enum Badge {
+
+    // Badges depending on score
+    BRONZE_MULTIPLICATOR,
+    SILVER_MULTIPLICATOR,
+    GOLD_MULTIPLICATOR,
+
+    // Other badges won for different conditions
+    FIRST_ATTEMPT,
+    FIRST_WON
+}
