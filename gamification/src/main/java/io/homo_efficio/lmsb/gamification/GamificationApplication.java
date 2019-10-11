4db0b6cdae7badab68bc9cfdbaf489c45c3c5da2
Homo Efficio <homo.efficio@gmail.com>
2019-10-11 18:53:15 +0900

    Add empty gamification submodule

---
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/GamificationApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.lmsb.gamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamificationApplication.class, args);
+    }
+
+}
---
+++ gamification/src/test/java/io/homo_efficio/lmsb/gamification/GamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.lmsb.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
