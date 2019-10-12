b23aa2d818e0b840a9be796f435a410f0d2d89f3
Homo Efficio <homo.efficio@gmail.com>
2019-10-12 12:14:17 +0900

    Add failing firstAttemptTest for GameServiceImpl [p146-Exercise]

--- gamification/src/test/java/io/homo_efficio/lmsb/gamification/service/GameServiceImplTest.java
+++ gamification/src/test/java/io/homo_efficio/lmsb/gamification/service/GameServiceImplTest.java
@@ -1,11 +1,20 @@
 package io.homo_efficio.lmsb.gamification.service;
 
+import io.homo_efficio.lmsb.gamification.domain.Badge;
+import io.homo_efficio.lmsb.gamification.domain.BadgeCard;
+import io.homo_efficio.lmsb.gamification.domain.GameStats;
+import io.homo_efficio.lmsb.gamification.domain.ScoreCard;
 import io.homo_efficio.lmsb.gamification.repository.BadgeCardRepository;
 import io.homo_efficio.lmsb.gamification.repository.ScoreCardRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 /**
  * @author homo.efficio@gmail.com
@@ -31,7 +40,21 @@ public class GameServiceImplTest {
 
     @Test
     public void firstAttemptBadgeTest() {
+        // given
+        Long userId = 3L;
+        Long attemptId = 31L;
+        given(scoreCardRepository.findByUserIdOrderByScoreTimestampDesc(userId))
+                .willReturn(Collections.emptyList());
+        BadgeCard badgeCard = new BadgeCard(userId, Badge.FIRST_ATTEMPT);
+        given(badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId))
+                .willReturn(Collections.singletonList(badgeCard));
 
+        // when
+        GameStats gameStats = gameService.newAttemptForUser(userId, attemptId, false);
+
+        // then
+        assertThat(gameStats.getBadges()).containsOnly(Badge.FIRST_ATTEMPT);
+        assertThat(gameStats.getScore()).isEqualTo(0);
     }
 
     @Test
