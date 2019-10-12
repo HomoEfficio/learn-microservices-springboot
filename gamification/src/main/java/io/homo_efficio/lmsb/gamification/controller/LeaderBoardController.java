bd3e01f49a86f357641259d10039d78335960161
Homo Efficio <homo.efficio@gmail.com>
2019-10-12 15:52:20 +0900

    Add getLeaders() impl for LeaderBoardController [4-17]

--- gamification/src/main/java/io/homo_efficio/lmsb/gamification/controller/LeaderBoardController.java
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/controller/LeaderBoardController.java
@@ -1,9 +1,14 @@
 package io.homo_efficio.lmsb.gamification.controller;
 
+import io.homo_efficio.lmsb.gamification.domain.LeaderBoardRow;
 import io.homo_efficio.lmsb.gamification.service.LeaderBoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author homo.efficio@gmail.com
@@ -15,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeaderBoardController {
 
     private final LeaderBoardService leaderBoardService;
+
+    @GetMapping
+    public ResponseEntity<List<LeaderBoardRow>> getLeaders() {
+        return ResponseEntity.ok(leaderBoardService.getCurrentLeaderBoard());
+    }
 }
