ad69aae27ff337c5dc2028a3c4e223bd08d7e231
Homo Efficio <homo.efficio@gmail.com>
2019-10-12 12:26:08 +0900

    Add FirstAttempt impl for GameServiceImpl [p146-Exercise]

--- gamification/src/main/java/io/homo_efficio/lmsb/gamification/service/GameServiceImpl.java
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/service/GameServiceImpl.java
@@ -1,10 +1,16 @@
 package io.homo_efficio.lmsb.gamification.service;
 
+import io.homo_efficio.lmsb.gamification.domain.Badge;
+import io.homo_efficio.lmsb.gamification.domain.BadgeCard;
 import io.homo_efficio.lmsb.gamification.domain.GameStats;
+import io.homo_efficio.lmsb.gamification.domain.ScoreCard;
 import io.homo_efficio.lmsb.gamification.repository.BadgeCardRepository;
 import io.homo_efficio.lmsb.gamification.repository.ScoreCardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author homo.efficio@gmail.com
@@ -19,7 +25,22 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct) {
-        return null;
+        List<ScoreCard> scoreCards = scoreCardRepository.findByUserIdOrderByScoreTimestampDesc(userId);
+        int score = 0;
+        if (correct) {
+
+        } else {
+            if (scoreCards.isEmpty()) {
+                BadgeCard badgeCard = new BadgeCard(userId, Badge.FIRST_ATTEMPT);
+                BadgeCard dbBadgeCard = badgeCardRepository.save(badgeCard);
+            }
+        }
+        List<BadgeCard> badgeCards = badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId);
+        List<Badge> badges = new ArrayList<>();
+        for (BadgeCard badgeCard: badgeCards) {
+            badges.add(badgeCard.getBadge());
+        }
+        return new GameStats(userId, score, badges);
     }
 
     @Override
