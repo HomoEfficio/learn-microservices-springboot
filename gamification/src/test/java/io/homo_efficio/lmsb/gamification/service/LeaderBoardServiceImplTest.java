2dea10b5860a77349f6cb4b2d4fca6c9c4e42531
Homo Efficio <homo.efficio@gmail.com>
2019-10-12 15:08:38 +0900

    Add empty test for LeaderBoardServiceImpl [p146-Exercise]

---
+++ gamification/src/test/java/io/homo_efficio/lmsb/gamification/service/LeaderBoardServiceImplTest.java
@@ -0,0 +1,12 @@
+package io.homo_efficio.lmsb.gamification.service;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-12
+ */
+public class LeaderBoardServiceImplTest {
+
+    // getCurrentLeaderBoard()는 Repository 조회 외에 별다른 로직이 없으므로 테스트 생략
+}
