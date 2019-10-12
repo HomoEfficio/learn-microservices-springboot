7935839092b6ba55fd5e6fe262fd2eddd5947c38
Homo Efficio <homo.efficio@gmail.com>
2019-10-12 11:22:49 +0900

    Add GameService [4-14]

---
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/service/GameService.java
@@ -0,0 +1,14 @@
+package io.homo_efficio.lmsb.gamification.service;
+
+import io.homo_efficio.lmsb.gamification.domain.GameStats;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-12
+ */
+public interface GameService {
+
+    GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct);
+
+    GameStats retrieveStatsForUser(Long userId);
+}
