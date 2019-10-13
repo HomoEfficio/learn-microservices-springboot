5398f3e2929a7590065f87a8a859357082884f3c
Homo Efficio <homo.efficio@gmail.com>
2019-10-14 00:12:05 +0900

    Add getStatsForUser impl for UserStatsController [4-18]

--- gamification/src/main/java/io/homo_efficio/lmsb/gamification/controller/UserStatsController.java
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/controller/UserStatsController.java
@@ -1,8 +1,12 @@
 package io.homo_efficio.lmsb.gamification.controller;
 
+import io.homo_efficio.lmsb.gamification.domain.GameStats;
 import io.homo_efficio.lmsb.gamification.service.GameService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,4 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserStatsController {
 
     private final GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<GameStats> getStatsForUser(@RequestParam("userId") Long userId) {
+        return ResponseEntity.ok(gameService.retrieveTotalStatsForUser(userId));
+    }
 }
