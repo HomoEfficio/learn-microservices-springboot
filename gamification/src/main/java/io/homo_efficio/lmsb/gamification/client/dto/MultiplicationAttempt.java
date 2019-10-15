fa32e4fe3d10b154a5f7943a4c75e886efd825bb
Homo Efficio <homo.efficio@gmail.com>
2019-10-16 00:18:26 +0900

    Add client MultiplicationAttempt [4-22]

---
+++ gamification/src/main/java/io/homo_efficio/lmsb/gamification/client/dto/MultiplicationAttempt.java
@@ -0,0 +1,32 @@
+package io.homo_efficio.lmsb.gamification.client.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.*;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-16
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+@JsonDeserialize(using = MultiplicationAttemptDeserializer.class)
+public class MultiplicationAttempt {
+
+    private final String userAlias;
+    private final int factorA;
+    private final int factorB;
+    private final int attempt;
+    private final boolean correct;
+
+    // For JSON/JPA
+
+    protected MultiplicationAttempt() {
+        this.userAlias = null;
+        this.factorA = -1;
+        this.factorB = -1;
+        this.attempt = -1;
+        this.correct = false;
+    }
+}
