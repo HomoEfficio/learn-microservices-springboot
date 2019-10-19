9087bde3f64eb0967a2931c6eb5a8e23ca459be7
Homo Efficio <homo.efficio@gmail.com>
2019-10-19 23:39:50 +0900

    Add CORS config to both microservices [5-4]
    - WebMvcConfigurerAdapter is deprecated in Spring 5
    - WebMvcConfigurer is used instead

---
+++ social-multiplication/src/main/java/io/homo_efficio/lmsb/socialmultiplication/config/WebConfig.java
@@ -0,0 +1,20 @@
+package io.homo_efficio.lmsb.socialmultiplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2019-10-19
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
