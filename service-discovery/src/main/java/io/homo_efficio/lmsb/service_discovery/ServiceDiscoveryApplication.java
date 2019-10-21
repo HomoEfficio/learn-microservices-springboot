d0381a1c65673de714bcde4bab34d84eca984c01
Homo Efficio <homo.efficio@gmail.com>
2019-10-21 23:39:30 +0900

    Add EnableEurekaServer [5-11]

--- service-discovery/src/main/java/io/homo_efficio/lmsb/service_discovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/io/homo_efficio/lmsb/service_discovery/ServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package io.homo_efficio.lmsb.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
