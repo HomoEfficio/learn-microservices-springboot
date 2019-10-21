c5796ea1af342303228aaf690167fe00d43ecc4e
Homo Efficio <homo.efficio@gmail.com>
2019-10-21 23:37:33 +0900

    Add Service Discovery Eureka [Figure 5-13-Equivalent]

---
+++ service-discovery/src/test/java/io/homo_efficio/lmsb/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.lmsb.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
